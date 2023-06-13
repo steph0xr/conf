16780f368236eb7489c3ae031580bd17ec3e4436
Stefano Offredi <stefano.offredi@gmail.com>
2023-06-13 14:50:15 +0200

    update

--- startup.sh
+++ startup.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-sudo apt install git i3 rcm tilix
+sudo apt install git curl i3 rcm tilix tmux kdiff3 make cmake gettext g++ fzf thunar zathura feh gnome-shell-extensions
 
 # set tilix as default
 # sudo update-alternatives --config x-terminal-emulator
+# dconf load /com/gexperts/Tilix/ < Tilix.ini
+
+# remove pwd when sudo:
+# sudo visudo
+# add line steph ALL=NOPASSWD: ALL
